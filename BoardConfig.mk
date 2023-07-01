24992ebfbf1718225e2d72498178ac37cafcadf8
Sarthak Roy <sarthakroy2002@gmail.com>
2023-07-01 17:14:30 +0700

    X6815D: Define Bootloader and Platform flags
    Change-Id: I8a4aad74ec90124cc35847c64d4db23f97af7ad7
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,5 +22,13 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a55
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := Infinix-X6815D
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6877
+BOARD_HAS_MTK_HARDWARE := true
+
 # Inherit the proprietary files
 include vendor/infinix/X6815D/BoardConfigVendor.mk
