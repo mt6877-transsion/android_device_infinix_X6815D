5736c91b9bfa4c978a4a1bc2d5a2841bf65cbb86
bengris32 <bengris32@protonmail.ch>
2023-11-18 17:16:22 +0000

    X6815D: Include hardware/mediatek soong namespace
    Change-Id: I2864b1e5d203cc52829f05e2154c617d9b548f0e
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -92,7 +92,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/infinix/X6815D/X6815D-vendor.mk)
