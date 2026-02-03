8925c3613de4397215d39ca695b8be526105c812
bengris32 <bengris32@protonmail.ch>
2026-02-03 08:54:37 +0100

    X6815D: Ship AOSP cgroups and task_profiles configs
    Change-Id: Ie74f85fa6328e6fa91d261c4aec257e5bb53ba0e
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -113,6 +113,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6.vendor:64 \
     android.hardware.camera.provider@2.6.vendor:64
 
+# Cgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
