a19c098fb9923b5a2adff796f676e212f67f9e89
vlxlxlv <fajarslebew31@gmail.com>
2026-08-13 17:29:48 +0200

    X6815D: Generated FCM for reporteed HALs
    Change-Id: Id4f04f0e7e811f545c299e39c912aa081c003b8a
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,6 +177,7 @@ BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 3
 
 # Vintf
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/device_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
