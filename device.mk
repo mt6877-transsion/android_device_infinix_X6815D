1b4728862b3067a7923976aebb094b59851be34c
vlxlxlv <fajarslebew31@gmail.com>
2026-08-13 15:51:59 +0200

    X6815D: Configure Bluetooth stack
    Change-Id: I75a0620be905c6f9bf08ab2e22263e2f2fcf5f3d
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -71,6 +71,14 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor
+
+PRODUCT_PACKAGES += \
+    vendor.mediatek.hardware.bluetooth.audio@2.1.vendor \
+    vendor.mediatek.hardware.bluetooth.audio@2.2.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
