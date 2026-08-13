d2df30befa706c2e2d8da0885f36d02815b83d9f
vlxlxlv <fajarslebew31@gmail.com>
2026-08-13 16:49:10 +0200

    X6815D: Build Lineage light services
    Change-Id: I97ad29e66b3e6f116c994d9897c7cd1d514a6305
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -115,6 +115,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.km41.xml
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.lineage
+
 # IMS
 $(call inherit-product, vendor/mediatek/ims/ims.mk)
 
