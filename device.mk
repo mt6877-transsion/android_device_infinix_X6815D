d28cbd4c39002eeec01655468bc03cc57a2117b4
vlxlxlv <fajarslebew31@gmail.com>
2026-08-18 07:54:33 +0200

    X6815D: Allow userspace reboots
    Change-Id: I90b16c5e5de8bc3000d70db5784bd955bfd97ff8
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Allow userspace reboots
+$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
